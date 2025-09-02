419d80fef225f4c80d634e6d192ad365e7349fe2
Henilkorat <henilkorat96@gmail.com>
2025-09-02 22:30:45 +0530

    Refresh

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -181,6 +181,20 @@ const DashboardPage = () => {
         </div>
       </div>
       <Separator />
+        <Button
+        className="mt-4"
+        variant="outline"
+        onClick={(e) => {
+          e.preventDefault();
+          fetchMessages(true);
+        }}
+      >
+        {isLoading ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <RefreshCcw className="h-4 w-4" />
+        )}
+      </Button>
       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
         {messages.length > 0 ? (
           messages.map((message, index) => (
